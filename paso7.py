0ed3c830418086b37edcdd7f767cb5283dc6de9c
Sebastian Marcos <sebasmarcos2107@gmail.com>
2025-09-08 21:31:59 -0300

    wip query 2 sql

--- paso7.py
+++ paso7.py
@@ -5,7 +5,11 @@ from pandasql import sqldf
 
 from modelos import df_paises, df_sedes, df_secciones, df_redes_sociales, df_gdp_per_capita_2023
 
-query_test = "SELECT COUNT(sede_id) FROM df_sedes WHERE country_code = 'USA'"
-query_vista_por_pais = "SELECT a.pais, COUNT(b.sede_id) AS cantidad_sedes, AVG(c.seccion_id) AS promedio_secciones_por_sede, gdp_pc_2023_usd AS pbi_2023 FROM df_paises a LEFT JOIN df_sedes b ON a.country_code = b.country_code LEFT JOIN df_secciones c ON b.sede_id = c.sede_id LEFT JOIN df_gdp_per_capita_2023 d ON a.country_code = d.country_code GROUP BY a.country_code"
+query_vista_por_pais = "SELECT a.pais, COUNT(DISTINCT b.sede_id) AS cantidad_sedes, CAST(COUNT(DISTINCT c.seccion_id) AS FLOAT) / COUNT(DISTINCT b.sede_id) AS promedio_secciones_por_sede, gdp_pc_2023_usd AS pbi_2023 FROM df_paises a LEFT JOIN df_sedes b ON a.country_code = b.country_code LEFT JOIN df_secciones c ON b.sede_id = c.sede_id LEFT JOIN df_gdp_per_capita_2023 d ON a.country_code = d.country_code GROUP BY a.country_code"
 vista_por_pais = sqldf(query_vista_por_pais)
-print(vista_por_pais)
+# print(vista_por_pais)
+
+query_vista_por_region = "SELECT a.region, COUNT(DISTINCT b.country_code) AS paises_con_sedes, SUM(DISTINCT c.gdp_pc_2023_usd) AS promedio_pbi_2023 FROM df_paises a LEFT JOIN df_sedes b ON a.country_code = b.country_code LEFT JOIN df_gdp_per_capita_2023 c ON a.country_code = c.country_code GROUP BY a.region"
+vista_por_region = sqldf(query_vista_por_region)
+print(vista_por_region)
+
